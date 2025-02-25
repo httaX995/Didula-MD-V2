ab85951453d99d3274b46578cb968d648eec031b
httaX995 <httx995@gmail.com>
2025-02-25 23:04:50 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,15 +6,15 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID === undefined ? 'PRABATH-MD~KfIzmAqT#_cXjtq9_qmQlCz0NZ2aHQ_8tcfLTMuVIIFeOo7C8B1k' : process.env.SESSION_ID,
+    SESSION_ID: process.env.SESSION_ID === undefined ? 'PRABATH-MD~lNAE2DBA#eeNiW3g45FWBBmyhQwgW97J_NJHWKtEp--Vbkpi7GRA' : process.env.SESSION_ID,
     PREFIX: process.env.PREFIX || '.',
     FOOTER: process.env.FOOTER || "*✦𝐃𝐢𝐝𝐮𝐥𝐚 𝐌𝐃 𝐕𝟐✦*",
     ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/za6ytm.jpg",
     PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 HEART_REACT: process.env.HEART_REACT || 'false',
-    SUDO: process.env.SUDO === undefined ? '94741671668,94771820962' : process.env.SUDO,
+    SUDO: process.env.SUDO === undefined ? '94779510013,94722770762' : process.env.SUDO,
     ALIVE_MSG: process.env.ALIVE_MSG === undefined ? "*Hello, I am alive now!!*" : process.env.ALIVE_MSG,
-    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ? "true" : process.env.AUTO_READ_STATUS,
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ? "false" : process.env.AUTO_READ_STATUS,
     MODE: process.env.MODE === undefined ? "public" : process.env.MODE,
     ANTI_BAD: process.env.ANTI_BAD === undefined ? "false" : process.env.ANTI_BAD,
     ANTI_LINK: process.env.ANTI_LINK === undefined ? "false" : process.env.ANTI_LINK,
